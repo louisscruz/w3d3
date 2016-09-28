81c563c6bc1094914d697b85b0b717d0bc8217de
App Academy Student <student@appacademy.io>
2016-09-28 15:51:54 -0700

    build CLI

--- URLShortener/app/models/shortened_url.rb
+++ URLShortener/app/models/shortened_url.rb
@@ -10,9 +10,7 @@ class ShortenedUrl < ActiveRecord::Base
     class_name: :Visit
 
   has_many :visitors,
-    # primary_key: :id,
-    # foreign_key: :shortened_url_id,
-    # class_name: :Visit
+      -> { distinct },
     through: :visits,
     source: :visitors
 
@@ -27,5 +25,17 @@ class ShortenedUrl < ActiveRecord::Base
 
   def self.create_for_user_and_long_url!(user, long_url)
     create!(long_url: long_url, short_url: self.random_code, user_id: user.id)
+  end
+
+  def num_clicks
+    visits.count
+  end
+
+  def num_uniques
+    visits.select(:user_id).distinct.count
+  end
+
+  def num_recent_uniques
+    visits.select(:user_id).distinct.where("created_at > ?", 10.minutes.ago).count
   end
 end
